4ef5d998c15cb7d0c6023d5f8affc49cb58afe67
Deden Rohman Gumilang <dedenrg@gmail.com>
2011-03-29 14:53:21 +0700

    ubah layout

--- protected/views/layouts/admin.php
+++ protected/views/layouts/admin.php
@@ -1,34 +1,34 @@
 <?php $this->beginContent('//layouts/main'); ?>
 
-<div id="main">
-	<div class="container" id="admin-container">
-		<div class="span-5" id="sidebar">
-			<h3><?php echo Yii::t('app','Menu') ?></h3>
-			<?php $this->widget('SideMenu', array(
-				'items'=>array(
-					array('label' => Yii::t('app','Berita'),'url' => array('/admin/berita/index')),
-					array('label' => Yii::t('app','Fakultas'),'url' => array('/admin/fakultas/index')),
-					array('label' => Yii::t('app','Jurusan / Program Studi'),'url' => array('/admin/jurusan/index')),
-					array('label' => Yii::t('app','Jenjang Studi'),'url' => array('/admin/jenjang/index')),
-					array('label' => Yii::t('app','Kabupaten'),'url' => array('/admin/kabupaten/index')),
-					array('label' => Yii::t('app','Kecamatan'),'url' => array('/admin/kecamatan/index')),
-					array('label' => Yii::t('app','Program KKN'),'url' => array('/admin/programKkn/index')),
-					array('label' => Yii::t('app','Kelompok / Lokasi'),'url' => array('/admin/kelompok/index')),
-					array('label' => Yii::t('app','User'),'url' => array('/admin/user/index')),
-					array('label' => Yii::t('app','Dosen'),'url' => array('/admin/dosen/index')),
-					array('label' => Yii::t('app','Mahasiswa'),'url' => array('/admin/mahasiswa/index')),
-					//array('label'=>Yii::t('app','Statistik'),'url'=>array('/admin/statistik/index')),
-				),
-				'htmlOptions'=>array('class'=>'sidemenu'),
-			));?>
-			
-		</div><!-- sidebar -->
-		<div class="span-19 last">
-			<div id="content">
-				<?php echo $content; ?>
-			</div><!-- content -->
-		</div>
-	
-	</div>
+<div id="main-admin">
+    <div class="container" id="admin-container">
+        <div class="span-5" id="sidebar">
+            <h3><?php echo Yii::t('app','Menu') ?></h3>
+            <?php $this->widget('SideMenu', array(
+                'items'=>array(
+                    array('label' => Yii::t('app','Berita'),'url' => array('/admin/berita/index')),
+                    array('label' => Yii::t('app','Fakultas'),'url' => array('/admin/fakultas/index')),
+                    array('label' => Yii::t('app','Jurusan / Program Studi'),'url' => array('/admin/jurusan/index')),
+                    array('label' => Yii::t('app','Jenjang Studi'),'url' => array('/admin/jenjang/index')),
+                    array('label' => Yii::t('app','Kabupaten'),'url' => array('/admin/kabupaten/index')),
+                    array('label' => Yii::t('app','Kecamatan'),'url' => array('/admin/kecamatan/index')),
+                    array('label' => Yii::t('app','Program KKN'),'url' => array('/admin/programKkn/index')),
+                    array('label' => Yii::t('app','Kelompok / Lokasi'),'url' => array('/admin/kelompok/index')),
+                    array('label' => Yii::t('app','User'),'url' => array('/admin/user/index')),
+                    array('label' => Yii::t('app','Dosen'),'url' => array('/admin/dosen/index')),
+                    array('label' => Yii::t('app','Mahasiswa'),'url' => array('/admin/mahasiswa/index')),
+                    //array('label'=>Yii::t('app','Statistik'),'url'=>array('/admin/statistik/index')),
+                ),
+                'htmlOptions'=>array('class'=>'sidemenu'),
+            ));?>
+            
+        </div><!-- sidebar -->
+        <div class="span-19 last">
+            <div id="content">
+                <?php echo $content; ?>
+            </div><!-- content -->
+        </div>
+    
+    </div>
 </div>
 <?php $this->endContent(); ?>
--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,11 +1,11 @@
 <?php
 $this->pageTitle=Yii::app()->name . ' - Error';
 $this->breadcrumbs=array(
-	'Error',
+    'Error',
 );
 ?>
 
-<h2>Error <?php echo $code; ?></h2>
+<h2 class="error">Error <?php echo $code; ?></h2>
 
 <div class="error">
 <?php echo CHtml::encode($message); ?>
